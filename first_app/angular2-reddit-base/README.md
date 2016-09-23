99e29dbb2d21a9a3397c0d658edd675f56e1fd24
Dale <drcorns@hotmail.com>
2016-09-22 23:59:08 -0700

    basic angular2 app

--- first_app/angular2-reddit-base/README.md
+++ first_app/angular2-reddit-base/README.md
@@ -1,2 +1,6 @@
 #angular2-studies
 Working through ng-book2
+####Branch helloworld
+import {*dependencies*} from '*someModule*' --*(destructuring)* , Component Annotation, Decorating the Class, Component Selector, Component template, Component Definition Class, backticks, class metadata<br/>
+App configuration, @NgModule Keys (*declarations, imports, and bootstrap*), Creating the application class, Running the app with .bootstrapModule in a browser with 'ahead of time' compiling. systemjs module loader. Serving the app with `npm run serve`.<br/>
+Component Class properties and constructor. Template tags *{{}}* and binding to the class. Typing variables with typescript. Working with arrays and `*ngfor`
--- first_app/angular2-reddit-base/app.ts
+++ first_app/angular2-reddit-base/app.ts
@@ -1,1 +1,36 @@
-// your code goes here
+import{
+  NgModule,
+  Component
+} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+@Component({//component annotation, decorating the class as a component, adding class metadata. Every annotation adds metadata to the class it precedes.
+  selector: 'hello-world',//<hello-world></hello-world> appearing in html will be compiled using the component class (HelloWorld)
+  template: `
+    <div>
+        Hello World
+        <div>Hello {{name}}!</div>
+    </div>
+    <ul>
+        <li *ngFor="let name of names">Hello {{name}}</li>
+    </ul>
+    `
+})
+class HelloWorld{ //Component Definition Class
+  name: string;
+  names: Array<string>; //or string[]
+  constructor(){
+    this.name = "George";
+    this.names = ['Peter', 'Paul', 'Mary', 'Lisa'];
+  }
+}
+
+@NgModule({//Create an NgModule that points to the HelloWorld component
+  declarations: [HelloWorld], //defines the components in the module/application, in this case only HelloWorld
+  imports: [BrowserModule], //describes the module/application dependencies
+  bootstrap: [HelloWorld] //The top level component for the module/application
+})
+
+class HelloWorldAppModule {}//The application class
+platformBrowserDynamic().bootstrapModule(HelloWorldAppModule);
