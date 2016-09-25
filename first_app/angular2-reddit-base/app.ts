5e08b99231a6753cd56a15aa972887160524768d
Dale <drcorns@hotmail.com>
2016-09-24 18:47:33 -0700

    build base

--- first_app/angular2-reddit-base/app.ts
+++ first_app/angular2-reddit-base/app.ts
@@ -1,36 +1,30 @@
-import{
-  NgModule,
-  Component
-} from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-@Component({//component annotation, decorating the class as a component, adding class metadata. Every annotation adds metadata to the class it precedes.
-  selector: 'hello-world',//<hello-world></hello-world> appearing in html will be compiled using the component class (HelloWorld)
-  template: `
-    <div>
-        Hello World
-        <div>Hello {{name}}!</div>
-    </div>
-    <ul>
-        <li *ngFor="let name of names">Hello {{name}}</li>
-    </ul>
-    `
-})
-class HelloWorld{ //Component Definition Class
-  name: string;
-  names: Array<string>; //or string[]
-  constructor(){
-    this.name = "George";
-    this.names = ['Peter', 'Paul', 'Mary', 'Lisa'];
-  }
-}
-
-@NgModule({//Create an NgModule that points to the HelloWorld component
-  declarations: [HelloWorld], //defines the components in the module/application, in this case only HelloWorld
-  imports: [BrowserModule], //describes the module/application dependencies
-  bootstrap: [HelloWorld] //The top level component for the module/application
+@Component({
+    selector: 'reddit',
+    template: `<form class="ui large form segment">
+<h3 class="ui header">Add a Link </h3>
+<div class="field">
+<label for="title">Title</label>
+<input name="title">
+</div>
+<div class="field">
+<label for="link">Link: </label>
+<input name="link">
+</div>
+</form>`
 })
 
-class HelloWorldAppModule {}//The application class
-platformBrowserDynamic().bootstrapModule(HelloWorldAppModule);
+class RedditApp {}
+
+@NgModule({
+      declarations: [RedditApp],
+      imports: [BrowserModule],
+      bootstrap: [RedditApp]
+})
+
+class RedditAppModule{}
+
+platformBrowserDynamic().bootstrapModule(RedditAppModule);
